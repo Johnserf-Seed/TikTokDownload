33b27be76695a33a3b1cb62053ae97f663934bda
JohnserfSeed <JohnserfSeed@gmail.com>
2021-01-26 16:07:00 +0800

    修复无法抓取的问题

--- TikTokMulti.py
+++ TikTokMulti.py
@@ -19,14 +19,17 @@ def get_info(count,choose,uid):
     if key == '':
         key = re.findall('&sec_uid=(.*?)&',str(r.url))[0]
     print(key)
-    api_post_url = 'https://www.iesdouyin.com/web/api/v2/aweme/%s/?sec_uid=%s&count=%d' % (choose,key,count)
+    api_post_url = 'https://www.iesdouyin.com/web/api/v2/aweme/%s/?sec_uid=%s&count=%s&max_cursor=0&aid=1128&_signature=RuMN1wAAJu7w0.6HdIeO2EbjDc&dytk=' % (choose,key,str(count))
+    headers = {
+        'user-agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.104 Safari/537.36'
+    }
     i = 0
     result = []
     while result == []:
         i = i + 1
         print('---正在进行第 %d 次尝试...\r' % i)
         time.sleep(0.3)
-        response = requests.get(api_post_url)
+        response = requests.get(url = api_post_url,headers=headers)
         html = json.loads(response.content.decode())
         if html['aweme_list'] != []:
             result = html['aweme_list']
@@ -54,7 +57,7 @@ def video_info(count,result):
             nickname.append(str(result[i]['author']['nickname']))
             dynamic_cover.append(str(result[i]['video']['dynamic_cover']['url_list'][0]))
         except:
-            print('抓取失败....')
+            #print('抓取失败....')
             pass
     return author_list,video_list,aweme_id,nickname,dynamic_cover
 
@@ -104,9 +107,9 @@ if __name__ == "__main__":
     else:
         choose = 'like'
 
-    #uid = cf.get("url","url")
+    uid = cf.get("url","uid")
 
-    result = get_info(count,choose,cf.get("url","uid"))
+    result = get_info(count,choose,uid)
 
     author_list,video_list,aweme_id,nickname,dynamic_cover = video_info(count,result)
 
