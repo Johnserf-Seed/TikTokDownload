8d37c9302fb71876995322914007f8b4dfa2dcb7
JohnserfSeed <JohnserfSeed@gmail.com>
2021-01-28 11:42:02 +0800

    过滤windows非法文件名

--- TikTokMulti.py
+++ TikTokMulti.py
@@ -16,8 +16,8 @@ class TikTok():
         self.headers = {
             'user-agent': 'Mozilla/5.0 (Linux; Android 8.0; Pixel 2 Build/OPD3.170816.012) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Mobile Safari/537.36 Edg/87.0.664.66'
             }
-
-    def single_down(self,url):
+    #单视频下载
+    def single_down(self,url,save):
         key = re.findall('video/(\d+)/',url)[0]
         jx_url  = f'https://www.iesdouyin.com/web/api/v2/aweme/iteminfo/?item_ids={key}'    #官方接口
         jss = json.loads(requests.get(url = jx_url,headers=self.headers).text)
@@ -51,24 +51,24 @@ class TikTok():
         r=requests.get(url=video_url,headers=headers)
         try:
             #创建并检测下载目录是否存在
-            os.makedirs('.\\Download\\' + nickname)
+            os.makedirs(save + nickname)
         except:
             pass
-        print('视频',video_title,'   下载中')
-        with open(nickname +  "\\" + video_title +'.mp4','wb') as f:
+        print('视频 ',video_title,'   下载中\r')
+        with open(save + nickname +  "\\" + re.sub(r'[\\/:*?"<>|\r\n]+', "_", video_title) +'.mp4','wb') as f:
             f.write(r.content)
 
         #原声下载    
         r=requests.get(url=music_url,headers=headers)
-        print('音频',music_title,'    下载中')
-        with open(nickname +  "\\" + music_title + '.mp3','wb') as f:
+        print('音频 ',music_title,'    下载中\r')
+        with open(save + nickname +  "\\" + re.sub(r'[\\/:*?"<>|\r\n]+', "_", music_title) + '.mp3','wb') as f:
             f.write(r.content)
         input('....下载完成，按任意键退出....\r')
         sys.exit()
         return 
 
     #返回个人主页api数据
-    def get_info(self,count,mode,uid):
+    def get_info(self,count,mode,uid,save):
         #获取解码后原地址
         r = requests.get(url = self.Find(uid)[0])
         #single_url = 'https://www.iesdouyin.com/share/video/'
@@ -78,7 +78,7 @@ class TikTok():
             pass
         else:
             print('....为您下载单个视频....\r')
-            self.single_down(r.url)
+            self.single_down(r.url,save)
 
         #获取用户sec_uid
         key = re.findall('&sec_uid=(.*?)&',str(r.url))[0]
@@ -125,6 +125,7 @@ class TikTok():
                 pass
         return author_list,video_list,aweme_id,nickname,dynamic_cover
 
+    #匹配粘贴的url地址
     def Find(self,string): 
         # findall() 查找匹配正则表达式的字符串
         url = re.findall('http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+', string)
@@ -144,8 +145,8 @@ class TikTok():
                 music_url = str(js['item_list'][0]['music']['play_url']['url_list'][0])
                 music_title = str(js['item_list'][0]['music']['author'])
                 r=requests.get(music_url)
-                print('音频',music_title,'    下载中')
-                with open(save + mode + "\\" + nickname[i] + '\\' + music_title + '.mp3','wb') as f:
+                print('音频 ',music_title,'    下载中\r')
+                with open(save + mode + "\\" + nickname[i] + '\\' + re.sub(r'[\\/:*?"<>|\r\n]+', "_", music_title) + '.mp3','wb') as f:
                     f.write(r.content)
             except:
                 if music_url == '':
@@ -155,19 +156,19 @@ class TikTok():
             try:
                 video = requests.get(video_list[i])
                 #保存视频
-                print(author_list[i],'    下载中')
-                with open(save + mode + "\\" + nickname[i] + '\\' + author_list[i] + '.mp4','wb') as f:
+                print('视频 ',author_list[i],'    下载中\r')
+                with open(save + mode + "\\" + nickname[i] + '\\' + re.sub(r'[\\/:*?"<>|\r\n]+', "_", author_list[i]) + '.mp4','wb') as f:
                     f.write(video.content)
                 #保存视频动态封面
                 dynamic = requests.get(dynamic_cover[i])
-                with open(save + mode + '\\'+ nickname[i] + '\\' + author_list[i] + '.webp','wb') as f:
+                with open(save + mode + '\\'+ nickname[i] + '\\' + re.sub(r'[\\/:*?"<>|\r\n]+', "_", author_list[i]) + '.webp','wb') as f:
                     f.write(dynamic.content)
             except:    
                 pass
-        input('....下载完成，按任意键退出....')
-        sys.exit()
+        sys.exit(input('....下载完成，按任意键退出....'))
         return
 
+    #读取并生成配置
     def read_conf(self):
         if os.path.isfile("conf.ini") == True:
             pass
@@ -177,7 +178,7 @@ class TikTok():
                 cf = configparser.ConfigParser()
                 # 往配置文件写入内容
                 cf.add_section("url")
-                cf.set("url", "uid", "https://v.douyin.com/J7ECkmp/")
+                cf.set("url", "uid", "https://v.douyin.com/JcjJ5Tq/")
                 cf.add_section("music")
                 cf.set("music", "musicarg", "yes")
                 cf.add_section("count")
@@ -192,6 +193,7 @@ class TikTok():
             except:
                 input('....生成失败,请前往GItHub下载配置文件....')
                 sys.exit()
+
         #实例化读取配置文件
         cf = configparser.ConfigParser()
         #用utf-8防止出错
@@ -228,12 +230,13 @@ if __name__ == "__main__":
     )
     print("#" * 110)
     print('\r')
-
+    
+    #实例化TiTk
     TiTk = TikTok()
     #读取配置
     uid,count,save,mode = TiTk.read_conf()   
     #返回个人主页api数据
-    result = TiTk.get_info(count,mode,uid)
+    result = TiTk.get_info(count,mode,uid,save)
     #处理视频api数据
     author_list,video_list,aweme_id,nickname,dynamic_cover = TiTk.video_info(count,result)
     #下载全部资源
