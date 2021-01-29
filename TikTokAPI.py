0a9e8d0677a95fe779f45a9ea0af743c9e7d267a
Johnserf-Seed <2080979987@qq.com>
2021-01-29 13:36:18 +0800

    抖音API

---
+++ TikTokAPI.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+'''
+@Description:TikTokAPI
+@Date       :2021/01/29 13:34:54
+@Author     :JohnserfSeed
+@version    :1.0
+@License    :(C)Copyright 2017-2020, Liugroup-NLPR-CASIA
+@Mail       :johnserfseed@gmail.com
+'''
+
+import requests
+
