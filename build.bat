cc1053d5a8f5d8ab570abc5502c7add900d280f5
JohnserfSeed <40727745+Johnserf-Seed@users.noreply.github.com>
2022-03-02 10:49:51 +0800

    💻构建脚本

---
+++ build.bat
@@ -0,0 +1,9 @@
+@echo off
+echo Install Require
+pip install -r requirements.txt
+echo Build EXE version, Ctrl + C to Exit
+pause
+echo Bulid TikTokDMulti
+pyinstaller -F -i logo.ico --version-file API\TikTokMultiVersion.txt TikTokMulti.py
+echo Bulid TikTokDownload
+pyinstaller -F -i logo.ico --version-file API\TikTokDownloadVersion.txt TikTokDownload.py
