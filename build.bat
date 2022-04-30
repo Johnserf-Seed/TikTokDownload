7d9d2f2b92d43922c4f121223f44815d76b83967
JohnserfSeed <johnserf-seed@foxmail.com>
2022-04-30 23:31:06 +0800

    ⚾添加了无水印图集的打包命令

--- build.bat
+++ build.bat
@@ -1,10 +1,12 @@
 @echo off
 echo Install Require
 pip install -r requirements.txt
-echo Build EXE version, Ctrl + C to Exit
+echo Build EXE version, Press Ctrl + C to Exit
 pause
-echo Bulid TikTokDMulti
+echo Bulid TikTokMulti
 pyinstaller -F -i logo.ico --version-file API\TikTokMultiVersion.txt TikTokMulti.py
 echo Bulid TikTokDownload
 pyinstaller -F -i logo.ico --version-file API\TikTokDownloadVersion.txt TikTokDownload.py
+echo Bulid TikTokPic
+pyinstaller -F -i logo.ico --version-file API\TikTokPicVersion.txt TikTokPic.py
 pause
