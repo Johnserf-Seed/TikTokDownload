565e223241011bacd63470042dc779b4ff87e248
johnserf-seed <2080979987@qq.com>
2022-08-18 23:42:40 +0800

    🦍添加了所有脚本的打包命令

--- build.bat
+++ build.bat
@@ -9,4 +9,8 @@ echo Bulid TikTokDownload
 pyinstaller -F -i logo.ico --version-file API\TikTokDownloadVersion.txt TikTokDownload.py
 echo Bulid TikTokPic
 pyinstaller -F -i logo.ico --version-file API\TikTokPicVersion.txt TikTokPic.py
+echo Bulid TikTokTool
+pyinstaller -F -i logo.ico --version-file API\TikTokTool.txt TikTokTool.py
+echo Bulid TikTokGUI
+pyinstaller -F -w -i logo.ico --version-file API\TikTokGUIVersion.txt TikTokMultiGUI.py
 pause
