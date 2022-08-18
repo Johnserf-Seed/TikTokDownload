2ed69748cc8953fe5a841a8a66c54b8529ea18b3
johnserf-seed <2080979987@qq.com>
2022-08-18 23:30:05 +0800

    🐔按照pep8修改了一些格式

--- TikTokDownload.py
+++ TikTokDownload.py
@@ -8,11 +8,17 @@
 @License    :(C)Copyright 2017-2020, Liugroup-NLPR-CASIA
 @Mail       :johnserfseed@gmail.com
 '''
-import requests,re,json,sys,getopt
+
+import re
+import sys
+import json
+import getopt
+import requests
+
 from retrying import retry
 
 def printUsage():
-	print ('''
+    print('''
         使用方法: 1、添加为环境变量 2、输入命令
         -u<url 抖音复制的链接:https://v.douyin.com/JtcjTwo/>
         -m<music 是否下载音频,默认为yes可选no>
@@ -20,18 +26,21 @@ def printUsage():
         例如：TikTokDownload.exe -u https://v.douyin.com/JtcjTwo/ -m yes
 
     ''')
-    #TikTokDownLoad.exe --url=<抖音复制的链接> --music=<是否下载音频,默认为yes可选no>
+# TikTokDownLoad.exe --url=<抖音复制的链接> --music=<是否下载音频,默认为yes可选no>
+
 
 def Find(string):
     # findall() 查找匹配正则表达式的字符串
-    url = re.findall('http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+', string)
+    url = re.findall(
+        'http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+', string)
     return url
 
+
 def main():
-    urlarg=""
-    musicarg="yes"
+    urlarg = ""
+    musicarg = "yes"
     try:
-        opts, args = getopt.getopt(sys.argv[1:],"hu:m:",["url=","music="])
+        opts, args = getopt.getopt(sys.argv[1:], "hu:m:", ["url=", "music="])
     except getopt.GetoptError:
         printUsage()
         sys.exit(-1)
@@ -39,76 +48,81 @@ def main():
         if opts == []:
             printUsage()
             urlarg = str(input("请输入抖音链接:"))
-            return urlarg,musicarg
+            return urlarg, musicarg
     except:
         pass
-    for opt,arg in opts:
+    for opt, arg in opts:
         if opt == '-h':
             printUsage()
             sys.exit(-1)
         elif opt in ("-u", "--url"):
-            urlarg=arg
-        elif opt in ("-m","--music"):
-            musicarg=arg
-    return urlarg,musicarg
+            urlarg = arg
+        elif opt in ("-m", "--music"):
+            musicarg = arg
+    return urlarg, musicarg
+
 
 @retry(stop_max_attempt_number=3)
-def download(video_url,music_url,video_title,music_title,headers,musicarg):
-    #视频下载
+def download(video_url, music_url, video_title, music_title, headers, musicarg):
+    # 视频下载
     if video_url == '':
         print('[  提示  ]:该视频可能无法下载哦~\r')
         return
     else:
-        r=requests.get(url=video_url,headers=headers)
+        r = requests.get(url=video_url, headers=headers)
         if video_title == '':
             video_title = '[  提示  ]:此视频没有文案_%s\r' % music_title
-        with open(f'{video_title}.mp4','wb') as f:
+        with open(f'{video_title}.mp4', 'wb') as f:
             f.write(r.content)
             print('[  视频  ]:%s下载完成\r' % video_title)
 
     if music_url == '':
         print('[  提示  ]:下载出错\r')
-        #return
+        # return
     else:
-        #原声下载
+        # 原声下载
         if musicarg != 'yes':
             print('[  提示  ]:不下载%s视频原声\r' % video_title)
-            #return
+            # return
         else:
-            r=requests.get(url=music_url,headers=headers)
-            with open(f'{music_title}.mp3','wb') as f:
+            r = requests.get(url=music_url, headers=headers)
+            with open(f'{music_title}.mp3', 'wb') as f:
                 f.write(r.content)
                 print('[  音频  ]:%s下载完成\r' % music_title)
-            #return
+            # return
 
-def video_download(urlarg,musicarg):
-        headers = {
-            'user-agent': 'Mozilla/5.0 (Linux; Android 8.0; Pixel 2 Build/OPD3.170816.012) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Mobile Safari/537.36 Edg/87.0.664.66'
-        }
-        r = requests.get(url = Find(urlarg)[0])
-        key = re.findall('video/(\d+)?',str(r.url))[0]
-        jx_url  = f'https://www.iesdouyin.com/web/api/v2/aweme/iteminfo/?item_ids={key}'    #官方接口
-        js = json.loads(requests.get(url = jx_url,headers=headers).text)
 
-        try:
-            video_url = str(js['item_list'][0]['video']['play_addr']['url_list'][0]).replace('playwm','play')   #去水印后链接
-        except:
-            print('[  提示  ]:视频链接获取失败\r')
-            video_url = ''
-        try:
-            music_url = str(js['item_list'][0]['music']['play_url']['url_list'][0])
-        except:
-            print('[  提示  ]:该音频目前不可用\r')
-            music_url = ''
-        try:
-            video_title = str(js['item_list'][0]['desc'])
-            music_title = str(js['item_list'][0]['music']['author'])
-        except:
-            print('[  提示  ]:标题获取失败\r')
-            video_title = '视频走丢啦~'
-            music_title = '音频走丢啦~'
-        download(video_url,music_url,video_title,music_title,headers,musicarg)
+def video_download(urlarg, musicarg):
+    headers = {
+        'user-agent': 'Mozilla/5.0 (Linux; Android 8.0; Pixel 2 Build/OPD3.170816.012) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Mobile Safari/537.36 Edg/87.0.664.66'
+    }
+    r = requests.get(url=Find(urlarg)[0])
+    key = re.findall('video/(\d+)?', str(r.url))[0]
+    # 官方接口
+    jx_url = f'https://www.iesdouyin.com/web/api/v2/aweme/iteminfo/?item_ids={key}'
+    js = json.loads(requests.get(url=jx_url, headers=headers).text)
 
-if __name__=="__main__":
-    urlarg,musicarg = main()
-    video_download(urlarg,musicarg)
+    try:
+        video_url = str(js['item_list'][0]['video']['play_addr']
+                        ['url_list'][0]).replace('playwm', 'play')  # 去水印后链接
+    except:
+        print('[  提示  ]:视频链接获取失败\r')
+        video_url = ''
+    try:
+        music_url = str(js['item_list'][0]['music']['play_url']['url_list'][0])
+    except:
+        print('[  提示  ]:该音频目前不可用\r')
+        music_url = ''
+    try:
+        video_title = str(js['item_list'][0]['desc'])
+        music_title = str(js['item_list'][0]['music']['author'])
+    except:
+        print('[  提示  ]:标题获取失败\r')
+        video_title = '视频走丢啦~'
+        music_title = '音频走丢啦~'
+    download(video_url, music_url, video_title, music_title, headers, musicarg)
+
+
+if __name__ == "__main__":
+    urlarg, musicarg = main()
+    video_download(urlarg, musicarg)
--- TikTokPic.py
+++ TikTokPic.py
@@ -9,10 +9,16 @@
 @Github     :https://github.com/johnserf-seed
 @Mail       :johnserfseed@gmail.com
 '''
-import requests,re,json,sys,getopt,time,os
+import os
+import re
+import sys
+import json
+import time
+import getopt
+import requests
 
 def printUsage():
-    print ('''
+    print('''
                                                     TikTokPic V1.0.0
             使用说明：
                     1、本程序目前仅支持命令行调用，只能用于图集下载
@@ -29,6 +35,7 @@ def printUsage():
             注意：  目前已经支持app内分享短链和web端长链识别。
     ''')
 
+
 def out_Print():
     print(r'''
 ████████╗      ██╗      ██╗  ██╗       ████████╗        ██████╗        ██╗  ██╗        ██████╗        ██╗        ██████╗
@@ -38,7 +45,8 @@ def out_Print():
    ██║         ██║      ██║  ██╗          ██║          ╚██████╔╝       ██║  ██╗        ██║            ██║       ╚██████╗
    ╚═╝         ╚═╝      ╚═╝  ╚═╝          ╚═╝           ╚═════╝        ╚═╝  ╚═╝        ╚═╝            ╚═╝        ╚═════╝''')
 
-    #TikTokPic.exe --url=<抖音复制的链接> --music=<是否下载音频,默认为yes可选no>
+    # TikTokPic.exe --url=<抖音复制的链接> --music=<是否下载音频,默认为yes可选no>
+
 
 def Find(string):
     # findall() 查找匹配正则表达式的字符串
@@ -46,9 +54,9 @@ def Find(string):
 
 
 def get_args():
-    urlarg=""
+    urlarg = ""
     try:
-        opts, args = getopt.getopt(sys.argv[1:],"hu:m:",["url=","music="])
+        opts, args = getopt.getopt(sys.argv[1:], "hu:m:", ["url=", "music="])
     except getopt.GetoptError:
         printUsage()
         sys.exit(0)
@@ -71,6 +79,8 @@ def get_args():
     return urlarg
 
 # 获取当前时间戳
+
+
 def now2ticks(type):
     """
     @description  : 获取当前时间戳
@@ -86,56 +96,60 @@ def now2ticks(type):
         return str(int(round(time.time() * 1000)))
 
 # 下载图集
+
+
 def pic_download(urlarg):
-        headers = {
-            'user-agent': 'Mozilla/5.0 (Linux; Android 8.0; Pixel 2 Build/OPD3.170816.012) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Mobile Safari/537.36 Edg/87.0.664.66'
-        }
-        try:
-            r = requests.get(url = Find(urlarg)[0])
-        # 如果输入链接不正确，则重新输入
-        except Exception as error:
-            print('[  警告  ]:输入链接有误！\r')
+    headers = {
+        'user-agent': 'Mozilla/5.0 (Linux; Android 8.0; Pixel 2 Build/OPD3.170816.012) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Mobile Safari/537.36 Edg/87.0.664.66'
+    }
+    try:
+        r = requests.get(url=Find(urlarg)[0])
+    # 如果输入链接不正确，则重新输入
+    except Exception as error:
+        print('[  警告  ]:输入链接有误！\r')
+        urlarg = get_args()
+        while urlarg == '':
+            print('[  提示  ]:请重新输入图集链接!\r')
             urlarg = get_args()
-            while urlarg == '':
-                print('[  提示  ]:请重新输入图集链接!\r')
-                urlarg = get_args()
-            pic_download(urlarg)
-            
-        # 2022/05/31 抖音把图集更新为note
-        key = re.findall('note/(\d+)?',str(r.url))[0]
+        pic_download(urlarg)
 
-        # 官方接口
-        jx_url  = f'https://www.iesdouyin.com/web/api/v2/aweme/iteminfo/?item_ids={key}'
-        js = json.loads(requests.get(url = jx_url,headers = headers).text)
+    # 2022/05/31 抖音把图集更新为note
+    key = re.findall('note/(\d+)?', str(r.url))[0]
 
-        try:
-            creat_time = time.strftime("%Y-%m-%d %H.%M.%S", time.localtime(
-                js['item_list'][0]['create_time']))
+    # 官方接口
+    jx_url = f'https://www.iesdouyin.com/web/api/v2/aweme/iteminfo/?item_ids={key}'
+    js = json.loads(requests.get(url=jx_url, headers=headers).text)
 
-            pic_title = str(js['item_list'][0]['desc'])
-            nickname = str(js['item_list'][0]['author']['nickname'])
-            # 检测下载目录是否存在
-            if not os.path.exists('Download\\' + 'pic\\' + nickname):
-                os.makedirs('Download\\' + 'pic\\' + nickname)
-            for i in range(len(js['item_list'][0]['images'])):
-                # 尝试下载图片
-                try:
-                    pic_url = str(js['item_list'][0]['images'][i]['url_list'][0])
-                    picture=requests.get(url = pic_url,headers = headers)
-                    p_url = 'Download\\' + 'pic\\' + nickname + '\\' + creat_time + pic_title + '_' + str(i) + '.jpeg'# + now2ticks()
-                    with open(p_url,'wb') as file:
-                        file.write(picture.content)
-                        print('[  提示  ]:' + p_url + '下载完毕\r')
-                except Exception as error:
-                    print('[  错误  ]:'+ error + '\r')
-                    print('[  提示  ]:发生了点意外！\r')
-                    break
-        except Exception as error:
-            print('[  错误  ]:'+ error + '\r')
-            print('[  提示  ]:获取图集失败\r')
-            return
+    try:
+        creat_time = time.strftime("%Y-%m-%d %H.%M.%S", time.localtime(
+            js['item_list'][0]['create_time']))
 
-if __name__=="__main__":
+        pic_title = str(js['item_list'][0]['desc'])
+        nickname = str(js['item_list'][0]['author']['nickname'])
+        # 检测下载目录是否存在
+        if not os.path.exists('Download\\' + 'pic\\' + nickname):
+            os.makedirs('Download\\' + 'pic\\' + nickname)
+        for i in range(len(js['item_list'][0]['images'])):
+            # 尝试下载图片
+            try:
+                pic_url = str(js['item_list'][0]['images'][i]['url_list'][0])
+                picture = requests.get(url=pic_url, headers=headers)
+                p_url = 'Download\\' + 'pic\\' + nickname + '\\' + creat_time + \
+                    pic_title + '_' + str(i) + '.jpeg'  # + now2ticks()
+                with open(p_url, 'wb') as file:
+                    file.write(picture.content)
+                    print('[  提示  ]:' + p_url + '下载完毕\r')
+            except Exception as error:
+                print('[  错误  ]:' + error + '\r')
+                print('[  提示  ]:发生了点意外！\r')
+                break
+    except Exception as error:
+        print('[  错误  ]:' + error + '\r')
+        print('[  提示  ]:获取图集失败\r')
+        return
+
+
+if __name__ == "__main__":
     # 设置控制台大小
     os.system("mode con cols=120 lines=25")
     # 输出logo
