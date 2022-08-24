72cca04891186161bb4bd78117f821c521844327
johnserf-seed <2080979987@qq.com>
2022-08-24 23:18:11 +0800

    🛎更新短链

--- Util/Command.py
+++ Util/Command.py
@@ -46,7 +46,7 @@ class Command:
         使用说明：
                 1、本程序目前支持命令行调用和配置文件操作，GUI预览版本已经发布
                 2、命令行操作方法：1）将本程序路径添加到环境变量
-                                2）控制台输入 TikTokMulti -u https://v.douyin.com/JtcjTwo/
+                                2）控制台输入 TikTokMulti -u https://v.douyin.com/jqwLHjF/
 
                 3、配置文件操作方法：1）运行软件前先打开目录下 conf.ini 文件配置用户主页和音乐下载模式
                                 2）按照控制台输出信息操作
