60c0db1209e5784997a8e7d9eb6afec61da3fb29
johnserf-seed <2080979987@qq.com>
2022-08-30 10:23:47 +0800

    🔊添加复用函数功能

--- Util/__init__.py
+++ Util/__init__.py
@@ -31,6 +31,56 @@ from .Config import Config
 from .Command import Command
 from .Profile import Profile
 from .Download import Download
+from .Images import Images
 
 # 日志记录
 log = Log()
+
+headers = {
+            'user-agent': 'Mozilla/5.0 (Linux; Android 8.0; Pixel 2 Build/OPD3.170816.012) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Mobile Safari/537.36 Edg/87.0.664.66'
+        }
+
+def replaceT(obj):
+        """替换文案非法字符
+
+        Args:
+            obj (_type_): 传入对象
+
+        Returns:
+            new: 处理后的内容
+        """
+        # '/ \ : * ? " < > |'
+        reSub = r"[\/\\n\:\*\?\"\<\>\|]"
+        new = []
+        if type(obj) == list:
+            for i in obj:
+                # 替换为下划线
+                retest = re.sub(reSub, "_", i)
+                new.append(retest)
+        elif type(obj) == str:
+            obj.replace('\\','')
+            obj.replace('\/','')
+            obj.replace(':','')
+            obj.replace('*','')
+            obj.replace('?','')
+            obj.replace('<','')
+            obj.replace('>','')
+            obj.replace('|','')
+            obj.replace('"','')
+            new = obj.replace('\n','')
+            # 替换为下划线
+            # new = re.sub(reSub, "_", obj, 0, re.MULTILINE)
+        return new
+
+if (platform.system() == 'Windows'):
+    sprit = '\\'
+    # 💻
+    print('[   💻   ]:Windows平台')
+elif (platform.system() == 'Linux'):
+    sprit = '/'
+    # 🐧
+    print('[   🐧   ]:Linux平台')
+else:
+    sprit = '/'
+    # 🍎
+    print('[   🍎   ]:MacOS平台')
