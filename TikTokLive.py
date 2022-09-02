9e7be09f381023fd603c0fd8f406d1c21f53e82b
johnserf-seed <2080979987@qq.com>
2022-09-02 23:58:35 +0800

    🖥直播推流抓取
    占坑

---
+++ TikTokLive.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+'''
+@Description:TikTokLive.py
+@Date       :2022/08/19 18:48:37
+@Author     :JohnserfSeed
+@version    :1.0
+@License    :(C)Copyright 2019-2022, Liugroup-NLPR-CASIA
+@Github     :https://github.com/johnserf-seed
+@Mail       :johnserfseed@gmail.com
+-------------------------------------------------
+Change Log  :
+-------------------------------------------------
+'''
+
+# ld = 标清
+
+# sd = 高清
+
+# hd = 超清
+
+# uhd = 蓝光
+
+# or4 = 原画
