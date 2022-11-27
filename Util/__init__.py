42d4c8c4762c321bef69846c0424c6a656f12e0a
JohnserfSeed <johnserf-seed@foxmail.com>
2022-11-27 19:34:01 +0800

    🎳修改注释版本

--- Util/__init__.py
+++ Util/__init__.py
@@ -88,11 +88,11 @@ print('''
 print("#" * 120)
 print(
         """
-                                                TikTokDownload V1.3.0
+                                                TikTokTool V1.3.0
         使用说明：
                 1、本程序目前支持命令行调用和配置文件操作，GUI预览版本已经发布
                 2、命令行操作方法：1）将本程序路径添加到环境变量
-                                2）控制台输入 TikTokMulti -u https://v.douyin.com/jqwLHjF/
+                                2）控制台输入 TikTokTool -u https://v.douyin.com/jqwLHjF/
 
                 3、配置文件操作方法：1）运行软件前先打开目录下 conf.ini 文件配置用户主页和音乐下载模式
                                 2）按照控制台输出信息操作
