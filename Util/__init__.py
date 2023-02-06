6d82b42720dd05089d0b8205e0430e1e3dff4d35
JohnserfSeed <johnserf-seed@foxmail.com>
2023-02-06 21:39:17 +0800

    🧳添加使用说明

--- Util/__init__.py
+++ Util/__init__.py
@@ -125,7 +125,7 @@ print('''
 print("#" * 120)
 print(
     """
-                                                TikTokTool V1.3.0.43
+                                                TikTokTool V1.3.0.46
         使用说明：
                 1、本程序目前支持命令行调用和配置文件操作，GUI预览版本已经发布
                 2、命令行操作方法：1）将本程序路径添加到环境变量
@@ -136,6 +136,9 @@ print(
 
                 4、如有您有任何bug或者意见反馈请在 https://github.com/Johnserf-Seed/TikTokDownload/issues 发起
                 5、GUI预览版本现已发布，操作更简单 https://github.com/Johnserf-Seed/TikTokDownload/tags 下载
+                6、TikTokLive 输入抖音直播间web端链接，例如 https://live.douyin.com/176819813905
+                7、TikTokPic 
+                8、新版工具fastdl正在开发中 ----> https://github.com/Johnserf-Seed/fastdl
 
         注意：  目前已经支持app内分享短链和web端长链识别。
         """
