4aefce0622e674ea8e77460b98a5743561411b45
JohnserfSeed <johnserf-seed@foxmail.com>
2023-02-06 21:39:48 +0800

    🌚去除Multi的打包命令

--- build.bat
+++ build.bat
@@ -3,8 +3,6 @@ echo Install Require
 pip install -r requirements.txt
 echo Build EXE version, Press Ctrl + C to Exit
 pause
-echo Bulid TikTokMulti
-pyinstaller -F -i logo.ico --version-file API\TikTokMultiVersion.txt TikTokMulti.py
 echo Bulid TikTokDownload
 pyinstaller -F -i logo.ico --version-file API\TikTokDownloadVersion.txt TikTokDownload.py
 echo Bulid TikTokPic
