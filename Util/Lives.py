2b2b767056de9b15395f74d119a9f30d7f79c85e
JohnserfSeed <johnserf-seed@foxmail.com>
2023-02-22 21:45:26 +0800

    🚘给直播添加xb接口 #314

--- Util/Lives.py
+++ Util/Lives.py
@@ -22,16 +22,18 @@ class Lives():
 
         web_rid = live_url.replace('https://live.douyin.com/','')
         # 2023/02/06 https://live.douyin.com/webcast/room/web/enter/
-        live_api = 'https://live.douyin.com/webcast/room/web/enter/?aid=6383&device_platform=web&web_rid=%s' % (web_rid)
+
+        live_api = Util.Urls().LIVE + Util.XBogus('aid=6383&device_platform=web&web_rid=%s' % (web_rid)).params
 
         headers = {
-            'Cookie': 'msToken=tsQyL2_m4XgtIij2GZfyu8XNXBfTGELdreF1jeIJTyktxMqf5MMIna8m1bv7zYz4pGLinNP2TvISbrzvFubLR8khwmAVLfImoWo3Ecnl_956MgOK9kOBdwM=; odin_tt=6db0a7d68fd2147ddaf4db0b911551e472d698d7b84a64a24cf07c49bdc5594b2fb7a42fd125332977218dd517a36ec3c658f84cebc6f806032eff34b36909607d5452f0f9d898810c369cd75fd5fb15; ttwid=1%7CfhiqLOzu_UksmD8_muF_TNvFyV909d0cw8CSRsmnbr0%7C1662368529%7C048a4e969ec3570e84a5faa3518aa7e16332cfc7fbcb789780135d33a34d94d2'
-        }
+            'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/104.0.0.0 Safari/537.36',
+            'Cookie': 'ttwid=1%7Ch5y5kqyWCBm-83nsJVded8JSFUBP6_6NqHUmPoZPbsk%7C1677073080%7Ca5a22c1959315d00a8d89cf1d088fb4de5720e45c262d53f389ec0f6373c8475;'
+            }
 
         response = Util.requests.request("GET", live_api, headers=headers)
-        while response.text == '':
-            print('[   🎦   ]:获取直播信息失败，正在重新获取\r')
-            response = Util.requests.request("GET", live_api, headers=headers)
+        if response.text == '':
+            input('[   🎦   ]:获取直播信息失败，请从web端获取新ttwid\r')
+            exit()
         live_json = Util.json.loads(response.text)
 
         return live_json
