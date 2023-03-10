0074226d101e4186568917ca59f308f2eac577a8
JohnserfSeed <johnserf-seed@foxmail.com>
2023-03-10 17:10:09 +0800

    🍻修改直播推流获取

--- Util/Lives.py
+++ Util/Lives.py
@@ -24,17 +24,14 @@ class Lives():
         # 2023/02/06 https://live.douyin.com/webcast/room/web/enter/
 
         live_api = Util.Urls().LIVE + Util.XBogus('aid=6383&device_platform=web&web_rid=%s' % (web_rid)).params
-
-        odin_tt = 'a09d8eb0d95b7b9adb4b6fc6591918bfb996096967a7aa4305bd81b5150a8199d2e29ed21883cdd7709c5beaa2be3baa'
-
-        headers = {
-            'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/104.0.0.0 Safari/537.36',
-            'Cookie': f'ttwid={Util.generate_ttwid};{odin_tt}'
-            }
-
+        # 获取命令行参数
+        cmd = Util.Command()
+        # 获取headers
+        headers = Util.Cookies(cmd.setting()).dyheaders
         response = Util.requests.request("GET", live_api, headers=headers)
+        print(headers)
         if response.text == '':
-            input('[   🎦   ]:获取直播信息失败，请从web端获取新ttwid\r')
+            input('[   🎦   ]:获取直播信息失败，请从web端获取新ttwid填入配置文件\r')
             exit()
         live_json = Util.json.loads(response.text)
 
