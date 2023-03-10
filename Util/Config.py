0d5fc222c853a1e2bba0e55e0108dc672d4f2fa5
JohnserfSeed <johnserf-seed@foxmail.com>
2023-03-10 16:42:15 +0800

    🔭配置生成修改

--- Util/Config.py
+++ Util/Config.py
@@ -12,6 +12,7 @@
 Change Log  :
 2022/07/29 23:18:47 : Init
 2022/08/16 18:34:27 : Add log moudle
+2023/03/08 14:42:05 : Add cookie,interval,update; conf
 -------------------------------------------------
 '''
 
@@ -20,22 +21,24 @@ import Util
 class Config:
 
     def __init__(self):
-        self.headers = Util.headers
         self.default = {
             'uid': 'https://v.douyin.com/k9NXNcH/',
             'music': 'yes',
             'path': 'Download',
-            'mode': 'post'
+            'mode': 'post',
+            'cookie':'',
+            'interval':'0',
+            'update':'yes'
         }
 
     def check(self):
-        """检查配置文件，不存在就生成默认配置文件
-
+        """
+        检查配置文件，不存在就生成默认配置文件
         Returns:
             self.cf: 配置文件对象
         """
         # 实例化读取配置文件
-        self.cf = Util.configparser.ConfigParser()
+        self.cf = Util.configparser.RawConfigParser()
 
         if Util.os.path.isfile("conf.ini") == True:
             # 用utf-8防止出错
@@ -53,17 +56,24 @@ class Config:
                 # self.cf.set("path", "path", ".\\Download\\")
                 self.cf.add_section("mode")
                 self.cf.set("mode", "mode", "post")
+                self.cf.add_section("cookie")
+                self.cf.set("cookie", "cookie", "#从web端获取后粘贴")
+                self.cf.add_section("interval")
+                self.cf.set("interval", "interval", "0")
+                self.cf.add_section("update")
+                self.cf.set("update", "update", "yes")
                 with open("conf.ini", "w") as f:
                     self.cf.write(f)
                 print('[  提示  ]:生成成功!\r')
                 Util.log.info('[  提示  ]:生成成功!')
-            except Exception as e:
-                Util.log.error(e)
+            except Exception as writeiniError:
+                Util.log.error(writeiniError)
                 self.download()
         return self.cf
 
-    def download(self):
-        """下载配置文件
+    def download(self) -> None:
+        """
+        下载配置文件
         """
         try:
             print('[  提示  ]:从GitHub为您下载配置文件!\r')
@@ -72,11 +82,12 @@ class Config:
                 'https://cdn.jsdelivr.net/gh/Johnserf-Seed/TikTokDownload@main/conf.ini')
             with open("conf.ini", "w") as f:
                 f.write(r.content)
-            print('[  提示  ]:下载成功!\r')
-            Util.log.info('[  提示  ]:下载成功!')
-        except Exception as e:
+            print('[  提示  ]:下载配置成功!\r')
+            Util.log.info('[  提示  ]:下载配置成功!')
+        except Exception as iniError:
             print('[  提示  ]:下载失败，请检查网络!\r')
             Util.log.info('[  提示  ]:下载失败，请检查网络!')
+            Util.log.error(iniError)
         return
 
 if __name__ == '__main__':
