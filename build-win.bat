452337020524f0810a64df0ff9ef6b1ec2f40c18
JohnserfSeed <johnserf-seed@foxmail.com>
2023-08-04 21:50:41 +0800

    🌧修改打包脚本

--- build-win.bat
+++ build-win.bat
@@ -4,17 +4,11 @@ pip install -r requirements.txt
 echo Build EXE version, Press Ctrl + C to Exit
 pause
 echo Build Server
-pyinstaller -F -i logo.ico --distpath Util --version-file API\Server.txt Util\Server.py
-echo Bulid TikTokDownload
-pyinstaller -F -i logo.ico --version-file API\TikTokDownloadVersion.txt TikTokDownload.py
-echo Bulid TikTokPic
-pyinstaller -F -i logo.ico --version-file API\TikTokPicVersion.txt TikTokPic.py
+pyinstaller -F -i logo.ico --distpath Util --version-file API\Server.txt Util\algorithm\Server.py
 echo Bulid TikTokTool
 pyinstaller -F -i logo.ico --version-file API\TikTokTool.txt TikTokTool.py
 echo Bulid TikTokLive
 pyinstaller -F  -i logo.ico --version-file API\TikTokLive.txt TikTokLive.py
-echo Bulid TikTokGUI
-pyinstaller -F -w -i logo.ico --version-file API\TikTokGUIVersion.txt TikTokMultiGUI.py
 echo Bulid TikTokUpdata
 pyinstaller -F -i logo.ico --version-file API\TikTokUpdata.txt TikTokUpdata.py
 pause
