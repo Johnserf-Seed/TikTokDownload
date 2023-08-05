51d598198004d2f51fd0b216b6ee32efa5f571b2
JohnserfSeed <johnserf-seed@foxmail.com>
2023-08-05 22:15:55 +0800

    🔧修复导入库失败造成无法运行的错误 #486
    添加--hidden-import=charset_normalizer.md__mypyc即可

--- build-win.bat
+++ build-win.bat
@@ -4,11 +4,11 @@ pip install -r requirements.txt
 echo Build EXE version, Press Ctrl + C to Exit
 pause
 echo Build Server
-pyinstaller -F -i logo.ico --distpath Util --version-file API\Server.txt Util\algorithm\Server.py
+pyinstaller -F -i logo.ico --distpath Util --version-file API\Server.txt --hidden-import=charset_normalizer.md__mypyc Util\algorithm\Server.py
 echo Bulid TikTokTool
-pyinstaller -F -i logo.ico --version-file API\TikTokTool.txt TikTokTool.py
+pyinstaller -F -i logo.ico --version-file API\TikTokTool.txt --hidden-import=charset_normalizer.md__mypyc TikTokTool.py
 echo Bulid TikTokLive
-pyinstaller -F  -i logo.ico --version-file API\TikTokLive.txt TikTokLive.py
+pyinstaller -F  -i logo.ico --version-file API\TikTokLive.txt --hidden-import=charset_normalizer.md__mypyc TikTokLive.py
 echo Bulid TikTokUpdata
-pyinstaller -F -i logo.ico --version-file API\TikTokUpdata.txt TikTokUpdata.py
+pyinstaller -F -i logo.ico --version-file API\TikTokUpdata.txt --hidden-import=charset_normalizer.md__mypyc TikTokUpdata.py
 pause
