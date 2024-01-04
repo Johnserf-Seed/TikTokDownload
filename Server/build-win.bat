6a4f22001a4b59f9b51e79244ee17abdf658a129
JohnserfSeed <johnserf-seed@foxmail.com>
2024-01-04 18:20:25 +0800

    完善Server应用打包命令

--- Server/build-win.bat
+++ Server/build-win.bat
@@ -1,9 +1,9 @@
 @echo off
-echo Install Npm Require
-npm i
 echo Install Pip Require
 pip install -r requirements.txt
 echo Build EXE version, Press Ctrl + C to Exit
 echo Build Server
-pyinstaller -F -i f2-logo.ico --version-file Server.txt --hidden-import=charset_normalizer.md__mypyc Server.py
+pyinstaller -F -i ..\f2-logo.ico --distpath . --version-file Server.txt --hidden-import=charset_normalizer.md__mypyc Server.py
+echo Install Npm Require
+npm i
 pause
